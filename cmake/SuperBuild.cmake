e77977982f8ff75b3fa6a548a796a506c1e9659e
Your Name <you@example.com>
2021-11-05 00:09:57 +0100

    xz

---
+++ cmake/SuperBuild.cmake
@@ -0,0 +1,29 @@
+include (ExternalProject)
+
+message("Running SuperBuild.cmake")
+
+# Third party external projects
+ExternalProject_Add( third_party
+  SOURCE_DIR ${PROJECT_SOURCE_DIR}/third_party
+  INSTALL_COMMAND ""
+  ALWAYS ON
+  BUILD_ALWAYS ON
+  CONFIGURE_HANDLED_BY_BUILD ON
+  BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/third_party
+)
+
+# Main project
+#ExternalProject_Add( ep_src
+#  DEPENDS third_party
+#  SOURCE_DIR ${PROJECT_SOURCE_DIR}
+#  CMAKE_ARGS -DUSE_SUPERBUILD=OFF
+#  INSTALL_COMMAND ""
+#  BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}
+#)
+#add_dependencies(ep_src third_party)
+
+add_custom_target(dummy COMMAND echo "Dummy main project target")
+add_dependencies(dummy 
+  third_party
+  #ep_src
+)
---
+++ third_party/tbb_install_files.cmake
@@ -0,0 +1,9 @@
+# copy libs
+file(GLOB TBB_LIBRARY_FILES
+  ${TBB_LIBS_DIR}/libtbb*
+)
+
+file(COPY ${TBB_LIBRARY_FILES} DESTINATION ${DEST_DIR}/lib)
+
+# copy headers
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${TBB_INCL_DIR} ${DEST_DIR}/include/tbb)
