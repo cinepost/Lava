6c59698c47018348cbbd2cd56a49b2d08a173b04
Your Name <you@example.com>
2024-11-26 17:05:49 +0300

    xz

--- third_party/slang_download_premake.cmake
+++ third_party/slang_download_premake.cmake
@@ -1,6 +1,13 @@
 if(EXISTS ${OUTPUT_FILE})
     message("premake5 already downloaded, skipping ...")
 else()
+
+    if(WIN32)
+        set(PREMAKE_ARCHIVE ${SOURCE_DIR}/premake5.zip)
+    else()
+        set(PREMAKE_ARCHIVE ${SOURCE_DIR}/premake5.tar.gz)
+    endif()
+
     find_program( CHMOD_EXECUTABLE
         NAMES chmod
         NAMES_PER_DIR
@@ -8,7 +15,12 @@ else()
 
     file(DOWNLOAD 
         ${PREMAKE_URL}
-        ${OUTPUT_FILE}
+        ${PREMAKE_ARCHIVE}
+    )
+
+    file(ARCHIVE_EXTRACT 
+        INPUT ${PREMAKE_ARCHIVE}
+        DESTINATION ${SOURCE_DIR}
     )
     
     execute_process(COMMAND ${CHMOD_EXECUTABLE} u+x ${OUTPUT_FILE})
